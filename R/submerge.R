4bed548027437123ef54d0fc7064f1d7892d98f4
itchyshin <itchyshin@gmail.com>
2019-12-28 19:15:05 +1100

    making automatic renaming by mix argument

--- R/submerge.R
+++ R/submerge.R
@@ -5,20 +5,32 @@
 #' @description Merge two model results tables (orchard objects).
 #' @param object1  object of class orchard
 #' @param object2  object of class orchard
+#' @param mix If TRUE, it will add the number to the moderator name
 #' @authors Shinichi Nakagawa - s.nakagawa@unsw.edu.au
 #' @authors Daniel Noble - daniel.noble@anu.edu.au
 #' @return Returns a data frame
 #' @export
 #' 
-submerge <- function(object1, object2, ...){
+submerge <- function(object1, object2, ..., mix = FALSE){
   orchard_list <- list(object1, object2, ...)
   
+  len <- length(orchard_list)
   # merging tables
   tables <- lapply(orchard_list, function(x) x$mod_table)
   tables <- do.call("rbind", tables)
+  
   # merging data
+  ## checking moderator names are the same or not
   datas <- lapply(orchard_list, function(x) x$data)
   datas <- do.call("rbind", datas)
+  
+  # renaming 
+  if(mix == TRUE){
+  names <- lapply(orchard_list, function(x) x$data$moderator)
+  names <- as.vector(mapply(function(x, y) paste0(x, y), x = names, y = 1:len))
+  datas$moderator <- factor(names)
+  tables$name <- levels(factor(names))
+  }
   
   model_results <- list(mod_table = tables, data = datas)
   
